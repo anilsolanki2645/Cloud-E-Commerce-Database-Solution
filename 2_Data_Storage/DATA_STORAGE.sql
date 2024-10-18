4ab4f4c97ca252e359509c2d0d5995cc303b266c
Anil <anilsolanki1834@gmail.com>
2024-10-18 23:11:22 +0530

    Create DATA_STORAGE.sql file

---
+++ 2_Data_Storage/DATA_STORAGE.sql
@@ -0,0 +1,29 @@
+------------------------------------------------------------------------------------------
+                                    -- DATA STORAGE
+------------------------------------------------------------------------------------------
+
+------------------------------------------------------------------------------------------
+                            -- Sample CSV Data Generation:
+------------------------------------------------------------------------------------------
+
+/*
+For each table, you need to create a csv file and add few records in a CSV file to load data into the tables via S3. Here's a general structure for the CSV files:
+
+customers.csv
+order_items.csv
+orders.csv
+payments.csv
+products.csv
+reviews.csv
+shipping.csv
+
+Please create these CSV files with the required sample data. If you need help with specific data, I can assist with a sample data script attached in this repo.
+*/
+
+/*
+AWS Process for S3 Configuration:
+1. login to account
+2. create a S3 Bucket
+3. Create a Access Key if not Created
+4. load the file into s3
+*/
